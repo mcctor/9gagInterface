1e3f6943dd2a31ee46852a78d76db941ff1c7fc9
mcctor <markvictormwanga@gmail.com>
2019-06-14 16:59:57 +0300

    First Commit.

---
+++ Gag/gag.py
@@ -0,0 +1,14 @@
+
+class GagInterface:
+
+    def __init__(self):
+        pass
+
+    def __fetch_html(self):
+        pass
+
+    def __parse_html_data(self):
+        pass
+
+    def get_posts(self):
+        pass
